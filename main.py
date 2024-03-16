bb6f8620697f498a54f27bd7e40852cc2695e710
Bill Brown <angela_123@petalmail.com>
2024-03-16 18:17:50 +0800

    1.0-240316-RC2

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from flask import Flask,request,redirect,render_template,abort,session
+from flask import Flask,request,redirect,render_template,abort,session,flash
 from flask_bootstrap import Bootstrap
 from settings import *
-from datetime import datetime,timedelta
+from datetime import datetime
 from forms import *
 from markdown import markdown
 from lib import *
@@ -36,22 +36,22 @@ def index():
         dic = {}
     try:
         id = int(request.args.get("id",-1))
-        article = Article.query.get(id)
-        if article:
-            newest = Article.query.order_by(Article.id.desc()).limit(10).all()
-            Article.query.get(id).count += 1
-            db.session.commit()
-            return render_template("page.html",article=article,newest=newest,**dic)
-        elif not "id" in request.args:
-            newest = Article.query.order_by(Article.id.desc()).limit(12).all()
-            hot = Article.query.order_by(Article.count.desc()).limit(12).all()
-            recommend = Article.query.filter(Article.recommend == True).order_by(Article.id.desc()).all()
-            authors = User.query.order_by(User.count.desc()).limit(20).all()
-            return render_template("index.html",newest=newest,hot=hot,authors=authors,recommend=recommend,**dic)
-        else:
-            abort(404)
-    except Exception as e:
-        return str(e)
+    except ValueError:
+        abort(404)
+    article = Article.query.get(id)
+    if article:
+        newest = Article.query.order_by(Article.id.desc()).limit(10).all()
+        Article.query.get(id).count += 1
+        db.session.commit()
+        return render_template("page.html",article=article,newest=newest,**dic)
+    elif not "id" in request.args:
+        newest = Article.query.order_by(Article.id.desc()).limit(12).all()
+        hot = Article.query.order_by(Article.count.desc()).limit(12).all()
+        recommend = Article.query.filter(Article.recommend == True).order_by(Article.id.desc()).all()
+        authors = User.query.order_by(User.count.desc()).limit(10).all()
+        return render_template("index.html",newest=newest,hot=hot,authors=authors,recommend=recommend,**dic)
+    else:
+        abort(404)
 @app.route("/login/",methods=["GET","POST"])
 def login():
     if session.get("logged_in"):
@@ -71,9 +71,10 @@ def login():
                 session["logged_in"] = True
                 session["user"] = user.realname
                 session["uid"] = user.id
-                session["expire"] = datetime.now() + timedelta(days=1)
+                flash("登录成功","success")
                 return redirect("/")
             else:
+                flash("登录失败，可能因为密码错误或密码更改（以第一次登录本网站时为准）","danger")
                 return redirect("/login/")
         else:
             res = yx_login(yxid,password)
@@ -91,9 +92,10 @@ def login():
                 session["logged_in"] = True
                 session["user"] = res["data"]["userName"]
                 session["uid"] = int(yxid)
-                session["expire"] = datetime.now() + timedelta(days=1)
+                flash("登录成功","success")
                 return redirect("/")
             else:
+                flash("登录失败，可能因为密码错误或无法连接云校","danger")
                 return redirect("/login/")
     return render_template("login.html",form=form)
 @app.route("/write/",methods=["GET","POST"])
@@ -120,6 +122,7 @@ def write():
         User.query.filter(User.realname == author).update({"count":User.count + 1})
         db.session.add(article)
         db.session.commit()
+        flash("发布成功","success")
         return redirect(f"/?id={article.id}")
     return render_template("write.html",form=form,**dic)
 @app.route("/author/",methods=["GET"])
@@ -131,7 +134,7 @@ def author():
     if "id" in request.args:
         try:
             author = User.query.get(int(request.args.get("id")))
-        except:
+        except ValueError:
             abort(404)
         if author:
             articles = Article.query.filter(Article.author == author.realname).order_by(Article.id.desc()).all()
@@ -150,11 +153,67 @@ def recommend():
             art = Article.query.get(int(request.args.get("id")))
             art.recommend = not art.recommend
             db.session.commit()
+            flash("推荐成功","success")
             return redirect("/")
         else:
             abort(404)
     else:
         abort(404)
+@app.route("/change/",methods=["GET","POST"])
+def change():
+    if session.get("logged_in"):
+        dic = {"user":session.get("user"),"uid":session.get("uid")}
+    else:
+        flash("请先登录","warning")
+        return redirect("/login/")
+    if "id" in request.args:
+        try:
+            article = Article.query.get(int(request.args.get("id")))
+        except ValueError:
+            abort(404)
+    if article and article.author == dic["user"]:
+        form = WriteForm()
+        if form.validate_on_submit():
+            title = form.title.data
+            md = form.md.data
+            theme = form.theme.data
+            jumimg = form.jumimg.data
+            article.title = title
+            article.html = render_markdown(md)
+            article.theme = theme
+            article.jumimg = jumimg
+            db.session.commit()
+            flash("修改成功","success")
+            return redirect(f"/?id={article.id}")
+        form.title.default = article.title
+        form.md.default = article.html
+        form.theme.default = article.theme
+        form.jumimg.default = article.jumimg
+        form.process()
+        return render_template("write.html",form=form,**dic)
+    elif article:
+        flash("您不能修改别人发布的文章","danger")
+        return redirect(f"/?id={article.id}")
+    abort(404)
+@app.route("/delete/",methods=["GET"])
+def delete():
+    if not session.get("logged_in"):
+        flash("请先登录","warning")
+        return redirect("/login")
+    if "id" in request.args:
+        try:
+            article = Article.query.get(int(request.args.get("id")))
+        except ValueError:
+            abort(404)
+    if article and article.author == session.get("user"):
+        db.session.delete(article)
+        db.session.commit()
+        flash("删除成功","success")
+        return redirect("/")
+    elif article:
+        flash("您不能删除别人发布的文章","danger")
+        return redirect("/")
+    abort(404)
 @app.route("/about/",methods=["GET"])
 def about():
     if session.get("logged_in"):
